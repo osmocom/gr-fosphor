a77d1950e3593a20aa1dd1d36af177867a90d46b
Sylvain Munaut <tnt@246tNt.com>
2014-08-05 08:11:57 +0200

    gr-fosphor: Remove an old workaround for a swig bug
    This has been removed of Gnuradio iteself in 2012 and this seems to
    be causing issues for some OOT on some platforms ...
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- python/__init__.py
+++ python/__init__.py
@@ -23,24 +23,6 @@ This is the GNU Radio FOSPHOR module. Place your Python package
 description here (python/__init__.py).
 '''
 
-# ----------------------------------------------------------------
-# Temporary workaround for ticket:181 (swig+python problem)
-import sys
-_RTLD_GLOBAL = 0
-try:
-    from dl import RTLD_GLOBAL as _RTLD_GLOBAL
-except ImportError:
-    try:
-        from DLFCN import RTLD_GLOBAL as _RTLD_GLOBAL
-    except ImportError:
-        pass
-
-if _RTLD_GLOBAL != 0:
-    _dlopenflags = sys.getdlopenflags()
-    sys.setdlopenflags(_dlopenflags|_RTLD_GLOBAL)
-# ----------------------------------------------------------------
-
-
 # import swig generated symbols into the fosphor namespace
 from fosphor_swig import *
 
@@ -53,9 +35,3 @@ except ImportError:
     def wx_sink_c(*args, **kwargs):
         raise exc_info[0], exc_info[1], exc_info[2]
 #
-
-# ----------------------------------------------------------------
-# Tail of workaround
-if _RTLD_GLOBAL != 0:
-    sys.setdlopenflags(_dlopenflags)      # Restore original flags
-# ----------------------------------------------------------------
