65c7283981edeba8dc3b4ec4b873fa0985fc5e59
Sylvain Munaut <tnt@246tNt.com>
2021-01-08 13:11:47 +0100

    fosphor: Update all copyright notices headers
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- lib/fosphor/cmap_fallback.glsl
+++ lib/fosphor/cmap_fallback.glsl
@@ -3,20 +3,8 @@
  *
  * Color mapping shader - Fall back GLSL 1.0 compatibility version
  *
- * Copyright (C) 2013-2014 Sylvain Munaut
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (C) 2013-2021 Sylvain Munaut
+ * SPDX-License-Identifier: GPL-3.0-or-later
  *
  * Note (to make it clear): for the purpose of this license, any software
  * making use of this shader (or derivative thereof) is considered to be
