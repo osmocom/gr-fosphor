26f06ef630bbfba952aceb864d90828ca5a22a7b
Sylvain Munaut <tnt@246tNt.com>
2021-01-08 13:12:43 +0100

    gr-fosphor: Update all copyright notices headers
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- python/__init__.py
+++ python/__init__.py
@@ -1,22 +1,9 @@
+# Copyright 2008-2020 Free Software Foundation, Inc.
+# Copyright 2013-2021 Sylvain Munaut <tnt@246tNt.com>
 #
-# Copyright 2008,2009 Free Software Foundation, Inc.
+# This file is part of gr-fosphor
 #
-# This application is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3, or (at your option)
-# any later version.
-#
-# This application is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-
-# The presence of this file turns this directory into a Python package
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 # import swig generated symbols into the fosphor namespace
 from .fosphor_swig import *
